9192aa9f3518d5279af591f9e6097dc3304cceb5
prataplyf <prataplyf@gmail.com>
2020-08-13 14:27:36 +0530

    update cookies

--- cookies.py
+++ cookies.py
@@ -2,13 +2,13 @@ from flask import Flask, make_response, request
 
 app = Flask(__name__)
 
-@app.route('/set')
+@app.route('/setcookies')
 def setcookies():
     resp = make_response('setting cookies')
     resp.set_cookie('language','python')
     return resp
 
-@app.route('/get')
+@app.route('/getcookies')
 def getcookies():
     language = request.cookies.get('language')
     return 'selected language ' + language
