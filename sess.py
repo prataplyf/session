0678b490fe546ede03747074f6ed825f60b419a5
prataplyf <prataplyf@gmail.com>
2020-08-13 10:59:18 +0530

    session file

---
+++ sess.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "Hello_Ashish_thisismysecretkey"
+app.permanent_session_lifetime = timedelta(minutes=5)  # set lifetime of session
+
+@app.route('/')
+def home():
+    # check if session available or not
+    if "username" in session:
+        return redirect(url_for('user'))
+    else:
+        return render_template('index.html')
+
+# user login and create session
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        session.permanent = True  # setting session True which is valid for certain time
+        username = request.form['username']
+        password = request.form['password']
+        if username == "ashish" and password == "ashish@1":  # validate username and password
+            session['username'] = username  # create session and store username
+            return redirect(url_for('user'))  # redirect to the user page
+    else:
+        if "username" in session:  # check if session data present or not
+            return redirect(url_for('user'))  # redirect to the user page
+        return render_template('index.html')
+
+# if session is available then no need to login
+@app.route('/user')
+def user():
+    if "username" in session:
+        usr = session['username']
+        # logout = ""
+        return f"<h1> welcome {usr} </h1>"
+    else:
+        return redirect(url_for('login'))
+
+# destoy session
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
